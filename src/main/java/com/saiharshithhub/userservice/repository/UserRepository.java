c94f126152d78043724f93e07bebeeda54849b49
Saiharshith17 <eedarasaiharshith6699@gmail.com>
2025-01-23 23:49:06 +0530

    added

--- src/main/java/com/saiharshithhub/userservice/repository/UserRepository.java
+++ src/main/java/com/saiharshithhub/userservice/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.saiharshithhub.userservice.repository;
 
 import com.saiharshithhub.userservice.entities.UserInfoDto;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@EnableJpaRepositories
 public interface UserRepository extends CrudRepository<UserInfoDto, String> {
     UserInfoDto findByUserId(String userId);
 }
