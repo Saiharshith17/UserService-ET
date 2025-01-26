1918ef9272e5b3839f4a746ad9ca3e0ca9938a46
Saiharshith17 <eedarasaiharshith6699@gmail.com>
2025-01-26 23:21:32 +0530

    commited

--- src/main/java/com/saiharshithhub/userservice/entities/UserInfoDto.java
+++ src/main/java/com/saiharshithhub/userservice/entities/UserInfoDto.java
@@ -1,36 +1,36 @@
 package com.saiharshithhub.userservice.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+@Getter
+@Setter
 @Entity
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @JsonIgnoreProperties(ignoreUnknown=true)
 public class UserInfoDto {
-    Long id;
-
-
+    private Long id;
 
 
     @Id
+    @JsonProperty("user_id")
     private String userId;
 
+    @JsonProperty("first_name")
     private String firstName;// first_name is snakecase so we need to map them to camelcase (firstName)
-
+    @JsonProperty("last_name")
     private String lastName;
-
+    @JsonProperty("phone_number")
     private Long phoneNumber;
-
+    @JsonProperty("email")
     private String email;
-
+    @JsonProperty("profile_pic")
     private String profilePic;
 }
