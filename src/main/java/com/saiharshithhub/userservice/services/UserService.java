cde80bca43b5d1e2858c05c51d5601cb9fdc39bb
Saiharshith17 <eedarasaiharshith6699@gmail.com>
2025-01-27 23:45:56 +0530

    added changes from UserService

--- src/main/java/com/saiharshithhub/userservice/services/UserService.java
+++ src/main/java/com/saiharshithhub/userservice/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
 
 public class UserService {
 
@@ -14,16 +15,29 @@ public class UserService {
     private UserRepository userRepository;
 
     public UserInfoDto createOrUpdateUser(UserInfoDto userInfoDto){
-        Function<UserInfo, UserInfo> updatingUser= user->{
-            return userRepository.save(userInfoDto.transformToUserInfo());
+        UnaryOperator<UserInfo> updatingUser= user->{
+            //todo :add the update logic
+            return userRepository.save(user);
         };
         Supplier<UserInfo> createUser=( )->{
-            return userRepository.save(UserInfoDto.transformToUserInfo());
+            return userRepository.save(userInfoDto.transformToUserInfo());
         };
 
         UserInfo userInfo=userRepository.findByUserId(userInfoDto.getUserId())
                 .map(updatingUser)
-                .orElseGet(createUser);
+                .orElseGet(createUser); //takes supplier and gives the same
+        return new UserInfoDto(
+                userInfo.getUserId(),
+                userInfo.getFirstName(),
+                userInfo.getLastName(),
+                userInfo.getPhoneNumber(),
+                userInfo.getEmail(),
+                userInfo.getProfilePic()
+
+
+        );
+
+
     }
 
 }
