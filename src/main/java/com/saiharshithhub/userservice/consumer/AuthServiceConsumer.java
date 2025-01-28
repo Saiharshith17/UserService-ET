d6e6628dad5c74358f6429a5ab2296d9bb821019
Saiharshith17 <eedarasaiharshith6699@gmail.com>
2025-01-28 17:12:20 +0530

    updated authserviceconsumer

--- src/main/java/com/saiharshithhub/userservice/consumer/AuthServiceConsumer.java
+++ src/main/java/com/saiharshithhub/userservice/consumer/AuthServiceConsumer.java
@@ -18,6 +18,7 @@ public class AuthServiceConsumer {
 
 
 
+    @KafkaListener(topics="${spring.kafka.topic-json.name}",groupId="${spring.kafka.consumer.group-id}")
     public void listen(UserInfoDto eventData){
         try{
             //ToDo : Make it Transactional to handle idempotently and validate email,phonenumber  etc can use redis distributed lock
